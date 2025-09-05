12a73b24155082b632597d67a9038da40361bf87
Vishank Shukla <vishankshukla51@gmail.com>
2025-09-05 13:44:57 +0530

    Update GCD.cpp

--- basic math concept/GCD.cpp
+++ basic math concept/GCD.cpp
@@ -1,1 +1,17 @@
 
+// GDC 
+void gdc(int n,int m){
+    int k=abs(n-m);
+    int greatest;
+    for(int i=1;i<k;k++){
+        if(n%i==0 && m%i==0) greatest=i;
+    }
+    cout<< "GDC =" << greatest;
+}
+int main(){
+    int n;
+    cout <<  "enter the number:";
+    cin >>n;
+    gdc(n);
+    return 0;
+}
