bc4f959956bcf1f3585e53f7bd505e6c529a6baa
Vishank Shukla <vishankshukla51@gmail.com>
2025-09-05 13:24:36 +0530

    count_the_digit.cpp

--- basic math concept/count_the_digit.cpp
+++ basic math concept/count_the_digit.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int countDigits(int n) {
-    int count = 0;
-    if (n == 0) return 1;
-    while (n != 0) {
+// count digit in the nummber 
+int count_no(int n){
+    int count=0;
+    while(n>0){
         count++;
-        n /= 10;
+        n=n/10;
     }
     return count;
 }
 
-int main() {
-    int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "Number of digits: " << countDigits(num) << endl;
+int main(){
+    int n;
+    cout <<  "enter the number:";
+    cin >>n;
+    cout<< count_no(n);
     return 0;
 }
