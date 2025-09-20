4c52b9961016f1080ad455e581aef89ddab3ce87
Vishank Shukla <vishankshukla51@gmail.com>
2025-09-20 23:14:23 +0530

    array is sorted

--- Arrays Problem/Easy/is_sorted.cpp
+++ Arrays Problem/Easy/is_sorted.cpp
@@ -1,1 +1,26 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+bool CheckSorted(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int size;
+    cout << "Enter the size of the array: ";
+    cin>>size;
+    int arr[size];
+    cout << "Enter the element of the array: ";
+    for(int i=0;i<size;i++){
+        cin>>arr[i];
+    }
+    bool isSorted= CheckSorted(arr,size);
+    if(isSorted) cout<< "Array is Sorted";
+    else cout<< "Array is Not Sorted";
+   
+}
