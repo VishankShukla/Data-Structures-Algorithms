5bc0b47b3ccf6a201d417e6959808d1327c80d82
Vishank Shukla <vishankshukla51@gmail.com>
2026-08-21 00:48:42 +0530

    Odd Even Linked List

---
+++ Leetcode Problems/Mid/OddEvenLinkedList.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while (even != NULL && even->next != NULL) {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+
+            odd = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+
+        return head;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val;
+
+        if (head->next != NULL)
+            cout << " -> ";
+
+        head = head->next;
+    }
+
+    cout << endl;
+}
+
+int main() {
+
+    // 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    Solution obj;
+    head = obj.oddEvenList(head);
+
+    cout << "Odd-Even List: ";
+    printList(head);
+
+    return 0;
+}
